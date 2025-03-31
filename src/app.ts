bbd16f54fdd7cdb4561bb719a84a39c9d306fe06
ChronoKross <nathanblainegwyn@gmail.com>
2025-03-31 03:10:42 -0400

    updated cors to deploy basic demo of MVP

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors";
 import queueRouter from "./routes/queueRouter";
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://volunteer-frontend.onrender.com"
+];
+
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+origin: allowedOrigins
+}));
 app.use(express.json());
 
 // Mount route
